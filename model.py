15a4eb33768a1bdc176ec293f51b57780ee6b34b
Rui  Vieira <ruidevieira@googlemail.com>
2017-11-28 10:00:30 +0000

    Added check for latest models in processing loop

--- model.py
+++ model.py
@@ -1,5 +1,4 @@
 class Model:
-
     def __init__(self, sc, als_model, version, data_version):
         self._sc = sc
         self._als_model = als_model
@@ -9,3 +8,7 @@ class Model:
     @property
     def als(self):
         return self._als_model
+
+    @property
+    def version(self):
+        return self._version
