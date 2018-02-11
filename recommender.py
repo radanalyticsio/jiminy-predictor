6c1d73921cf5a872be697449f489f52d7565a556
Rui  Vieira <ruidevieira@googlemail.com>
2018-02-11 18:31:14 +0000

    Removed `recommender` module (dead code)

--- recommender.py
+++
@@ -1,7 +0,0 @@
-
-class Model:
-    def __init__(self, als_model):
-        self._als_model = als_model
-
-    def predictSingle(self, user_id, product_id):
-        return self._als_model.predict(user=user_id, product=product_id)
