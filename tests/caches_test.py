8d06b84f878aedbf463dd3a2f4f41ca92d466623
Rui  Vieira <ruidevieira@googlemail.com>
2018-02-14 20:50:41 +0000

    Initial tests documentation

--- tests/caches_test.py
+++ tests/caches_test.py
@@ -1,15 +1,25 @@
-import unittest
-from nose.tools import assert_dict_equal
+"""cache_tests
 
+This module contains test for cache store implementations
+"""
 from caches import MemoryCache
+from nose.tools import assert_dict_equal
+import unittest
 
 
 class MemoryCacheTests(unittest.TestCase):
+    """Test suite for memory cache backend
+    """
 
     def setUp(self):
+        """Create a `MemoryCache` instance
+        """
         self.cache = MemoryCache()
 
     def get_test(self):
+        """Test if the prediction retrieved from cache is the same as the
+        stored one
+        """
         prediction = {'id': 1,
                       'products': [
                           {'id': 200, 'rating': 4.5},
