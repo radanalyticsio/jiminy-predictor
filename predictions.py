71c7df5bcc87cee33cf272998c3a759545240721
Rui  Vieira <ruidevieira@googlemail.com>
2018-03-26 23:50:27 +0100

    Removed duplicate `get_arg`

--- predictions.py
+++ predictions.py
@@ -10,19 +10,7 @@ import os
 import os.path
 from pyspark import sql as pysql
 import storage
-
-
-def get_arg(env, default):
-    """
-    Get environment variable value or default, if not set
-    :param env: The environment variable to read
-    :type env: string
-    :param default: Default value to return if not set
-    :type default: string
-    :return: Environment variable value of default if not set
-    :rtype string
-    """
-    return os.getenv(env) if os.getenv(env, '') is not '' else default
+from utils import get_arg
 
 
 def loop(request_q, response_q):
